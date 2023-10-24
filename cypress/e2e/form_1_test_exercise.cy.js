e9564f3448c95a3bd96757f74b0e2e1066e8d07e
mmeest <martin.maasik.001@mail.ee>
2023-10-24 21:56:17 +0300

    My comments about the commit

--- cypress/e2e/form_1_test_exercise.cy.js
+++ cypress/e2e/form_1_test_exercise.cy.js
@@ -9,13 +9,13 @@ Tasks done during VS Code live demo:
 1 and 2 - If you see this text and found file location in project tree, great job!
 3 - Find "MyPass123" - and then duplicate line 20 by using Shift + Alt + DownArrow
 4 - Uncomment lines 17 - 20
-5 - Find and replace username2 to username
+5 - Find and replace username to username
 6 - autoformat the code, using Shift + Alt + F
  */
 
 describe('This is first test suite', () => {
     it('User can submit data only when valid mandatory values are added', () => {
-        // cy.get('#username2').type('Something')
+        // cy.get('#username').type('Something')
         // cy.get('[data-testid="phoneNumberTestId"]').type('10203040')
         // cy.get('input[name="password"]').type('MyPass123')
         // cy.get('[name="confirm"]').type('MyPass123')
@@ -39,3 +39,4 @@ describe('This is first test suite', () => {
         cy.get('#success_message').should('have.css', 'display', 'block')
     });
 })
+// This is my first comment
